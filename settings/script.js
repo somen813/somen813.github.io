5cb4e81f195b898e603a86cabadbd699a6f55bc2
somen <sota2009@hotmail.com>
2026-03-22 18:56:14 +0900

    スマホ表示のアニメーションを作成

--- settings/script.js
+++ settings/script.js
@@ -2,6 +2,83 @@ const nowURL = window.location.href;
 const body = document.querySelector('body');
 const content = document.getElementById('content');
 
+function initHeader() {
+	const headerMenu = document.getElementById('headerMenu');
+	const headerToggle = document.getElementById('headerToggle');
+	const icon_menu = document.getElementById('icon_menu');
+	const icon_close = document.getElementById('icon_close');
+	const shareBtn_copy = document.getElementById('shareBtn_copy');
+	let headerMenuOpen = false;
+
+	function headerMenu_animation(direction) {
+		const keyframes = {
+			transform: ['translate(0, 0)', 'translate(100%, 0)'],
+		};
+		const options = {
+			duration: 250,
+			easing: 'ease',
+			fill: 'forwards',
+			direction: direction,
+		};
+		return headerMenu.animate(keyframes, options);
+	}
+	function headerToggle_animation(direction) {
+		const options = {
+			duration: 250,
+			easing: 'ease',
+			fill: 'forwards',
+			direction: direction,
+		}
+		function backgroundColor() {
+			const keyframes = {
+				backgroundColor: ['rgba(56, 123, 178, 1)', 'transparent'],
+			};
+			headerToggle.animate(keyframes, options);
+		}
+		function icon_menu_animation() {
+			const keyframes = {
+				opacity: [1, 0],
+			};
+			icon_menu.animate(keyframes, options);
+		}
+		function icon_close_animation() {
+			const keyframes = {
+				opacity: [0, 1],
+			};
+			icon_close.animate(keyframes, options);
+		}
+		backgroundColor();
+		icon_menu_animation();
+		icon_close_animation();
+	}
+	document.addEventListener('click', (e) => {
+		const clickHeaderToggle = e.target.closest('#headerToggle');
+		const clickOutSide = !e.target.closest('#headerMenu') && !clickHeaderToggle;
+		if (clickHeaderToggle) {
+			if (headerMenuOpen) {
+				headerMenu_animation('reverse');
+				headerToggle_animation('reverse');
+				headerMenuOpen = false;
+				return
+			};
+			headerMenu_animation('normal');
+			headerToggle_animation('normal');
+			headerMenuOpen = true;
+		};
+		if (clickOutSide && headerMenuOpen) {
+			headerMenu_animation('reverse');
+			headerToggle_animation('reverse');
+			headerMenuOpen = false;
+			return;
+		};
+	});
+
+
+	function copylink() {
+		navigator.clipboard.writeText(nowURL)
+	}
+	shareBtn_copy.addEventListener('click', copylink);
+}
 async function loadingHeader() {
 	let retries = 0;
 	const maxRetries = 3;
@@ -9,7 +86,8 @@ async function loadingHeader() {
 		try {
 			const responce = await fetch('/settings/header.html');
 			const data = await responce.text();
-			await body.insertAdjacentHTML('afterbegin', data)
+			await body.insertAdjacentHTML('afterbegin', data);
+			await initHeader();
 			return
 		} catch(error) {
 			retries++;
@@ -40,23 +118,6 @@ async function loadingCopyright() {
 }
 
 
-function initHeaderMenuAnimation() {
-	const headerMenuBox = document.getElementById('hedermenuBox');
-	const headerMenuToggle = document.getElementById('headerMenuToggle');
 
-	function headerMenuBoxAnimation(direction) {
-		const keyframs = {
-			translate: ['0 0', '-100% 0'],
-		};
-		const options = {
-			duration: 250,
-			easing: 'ease',
-			fill: 'forwards',
-			direction: direction,
-		};
-		return headerMenuBox.animate(keyframs, options);
-	}
-
-}
 loadingHeader();
 loadingCopyright();
